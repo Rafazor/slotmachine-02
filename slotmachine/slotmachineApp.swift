84c8dc3850a359ae48ae67cb24c95210a8baf18a
Călin Rafa <calin.rafa@alexishr.com>
2023-03-17 16:26:56 +0200

    Initial Commit

---
+++ slotmachine/slotmachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotmachineApp.swift
+//  slotmachine
+//
+//  Created by Calin Rafa on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct slotmachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
